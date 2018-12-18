b54a7dbcc9898a50c47fc4b89af1ecb8376c3e60
MickaelHm <harmeau.mickael@gmail.com>
2018-12-18 14:01:18 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6};
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            numbers.Add(7);
+            numbers.Insert(0, 1);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+
+        }
+    }
+}
